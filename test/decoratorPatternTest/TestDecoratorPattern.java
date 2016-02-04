e2d498f26580b535430bc22710bc11132f2fe5ce
Bo Peng <pengb@rose-hulman.edu>
2016-02-04 13:39:53 -0500

    Fixed test

--- test/decoratorPatternTest/TestDecoratorPattern.java
+++ test/decoratorPatternTest/TestDecoratorPattern.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import directory.reader.DirectoryReader;
 import problem.asm.DesignParser;
+import problem.asm.PatternParser;
 import umlMaker.app.UMLGenerator;
 import umlMaker.impl.UMLMakerOutputStream;
 import visitor.api.ITraverser;
@@ -15,18 +16,20 @@ import visitor.api.IVisitor;
 public class TestDecoratorPattern {
 	public static void main(String[] args) throws IOException {
 		DesignParser parser = new DesignParser();
-
+		PatternParser parser2 = new PatternParser();
+		
 		DirectoryReader reader = new DirectoryReader(
 				"D:\\Bo Peng\\Development\\CSSE374\\CSSE374V0id\\test", "decoratorPattern");
 
 		ArrayList<String> files = reader.readDirectory();
 
 		parser.main(files);
-
+		parser2.main(files, parser.model);
+		
 		OutputStream xmlOut = new FileOutputStream("docs/UMLTestAdapterPattern.txt");
 		IVisitor xmlWriter = new UMLMakerOutputStream(xmlOut);
 
-		ITraverser traverser = (ITraverser) parser.model;
+		ITraverser traverser = (ITraverser) parser2.model;
 		String title = "DecoratorPattern";
 		xmlOut.write("digraph ".getBytes());
 		xmlOut.write(title.getBytes());
