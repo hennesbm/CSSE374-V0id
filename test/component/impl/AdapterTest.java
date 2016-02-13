6c65c5c16cb235dfac4b2bea6a5cec23c55f367a
Chi Zhang <superzcdalian@gmail.com>
2016-02-13 14:41:49 -0500

    Delete Component.java and added Adapter Unit test

---
+++ test/component/impl/AdapterTest.java
@@ -0,0 +1,46 @@
+package component.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdapterTest {
+
+	private String className;
+	private String component;
+	private String adaptee;
+
+	Adapter a;
+
+	public AdapterTest() {
+		className = "CSSE374";
+		component = "code";
+		adaptee = "CSSE120";
+		this.a = new Adapter(className, component, adaptee);
+	}
+
+	@Test
+	public void testGetType() {
+		assertEquals("Adapter", a.getType());
+	}
+
+	@Test
+	public void testGetComponent() {
+		assertEquals(component, a.getComponent());
+	}
+
+	@Test
+	public void testGetColor() {
+		assertEquals("red", a.getColor());
+	}
+
+	@Test
+	public void testGetClassName() {
+		assertEquals(className, a.getClassName());
+	}
+
+	@Test
+	public void testGetAdaptee() {
+		assertEquals(adaptee, a.getAdaptee());
+	}
+}
