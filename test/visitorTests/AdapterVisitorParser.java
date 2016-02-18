f6507a313f42c9b4a5c9a3127420679c7a35ea4b
Chi Zhang <superzcdalian@gmail.com>
2016-02-17 20:29:06 -0500

    AdapterVistorParserTest

---
+++ test/visitorTests/AdapterVisitorParser.java
@@ -0,0 +1,38 @@
+package visitorTests;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Opcodes;
+
+import component.api.IModel;
+import problem.asm.AdapterVisitor;
+
+public class AdapterVisitorParser {
+	public static ArrayList<String> CLASSES;
+	public static String markedSelected;
+	public IModel model;
+
+	/**
+	 * Reads in a list of Java Classes and reverse engineers their design.
+	 *
+	 * @param args:
+	 *            the names of the classes, separated by spaces. For example:
+	 *            java DesignParser java.lang.String
+	 *            edu.rosehulman.csse374.ClassFieldVisitor java.lang.Math
+	 * @throws IOException
+	 */
+	public void main(ArrayList<String> files, IModel model) throws IOException {
+		AdapterVisitorParser.CLASSES = files;
+
+		this.model = model;
+
+		for (String className : CLASSES) {
+			ClassReader reader = new ClassReader(className);
+
+			AdapterVisitor av = new AdapterVisitor(Opcodes.ASM5, this.model);
+			reader.accept(av, ClassReader.EXPAND_FRAMES);
+		}
+	}
+}
